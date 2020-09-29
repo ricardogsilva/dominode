f890018eab700ed0f58a0c017fc76edcda60da77
Ricardo Garcia Silva <ricardo.garcia.silva@likeno.pt>
2020-09-29 13:15:21 +0100

    Add m2m signal handler to assign geoserver sync permission

--- geonode_dominode/geonode_dominode/signals.py
+++ geonode_dominode/geonode_dominode/signals.py
@@ -3,9 +3,9 @@
 import logging
 import typing
 
-from django.db.models.signals import post_save
-from django.contrib.auth.models import Group
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
+from geonode.groups.models import GroupProfile
 from guardian.shortcuts import assign_perm
 
 from .constants import GEOSERVER_SYNC_PERM_CODE
@@ -13,17 +13,23 @@ from .constants import GEOSERVER_SYNC_PERM_CODE
 logger = logging.getLogger(__name__)
 
 @receiver(
-    post_save,
-    sender=Group,
+    m2m_changed,
+    sender=GroupProfile.categories.through,
     dispatch_uid='assign_geoserver_sync_permission_uid'
 )
 def check_assign_geoserver_sync_permission(
         sender: typing.Type,
-        instance: Group,
-        created: bool,
+        instance: GroupProfile,
+        action: str,
+        reverse: bool,
         **kwargs
 ):
     """Assign GeoServer sync perm when a suitable Group instance is created."""
-    if created and instance.name.endswith('-editor'):
-        logger.debug(f'Assigning geoserver sync permission to {instance}...')
-        assign_perm(GEOSERVER_SYNC_PERM_CODE, instance)
+    if action == 'post_add':
+        existing_categories = [cat.name for cat in instance.categories.all()]
+        if instance.categories.filter(name='dominode-editor').exists():
+            group = instance.group
+            logger.debug(
+                f'Assigning geoserver sync permission to group {group}...')
+            assign_perm(
+                f'groups.{GEOSERVER_SYNC_PERM_CODE}', group, instance)
--- geonode_dominode/geonode_dominode/views.py
+++ geonode_dominode/geonode_dominode/views.py
@@ -1,19 +1,14 @@
 import logging
 
 from django.contrib import messages
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
-from django.http import (
-    HttpResponseBadRequest,
-    HttpResponseRedirect,
-)
+from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.http import require_POST
 from geonode.groups import views
 from geonode.groups.models import GroupProfile
 from guardian.decorators import permission_required_or_403
 
-from geonode_dominode.tasks import task_sync_geoserver
+from .tasks import task_sync_geoserver
 from .constants import (
     GEOSERVER_SYNC_PERM_CODE,
 )
@@ -23,12 +18,7 @@ logger = logging.getLogger('geonode_dominode')
 
 
 class GroupDetailView(views.GroupDetailView):
-    """Mixes a detail view (the group) with a ListView (the members)."""
-
-    # model = get_user_model()
     template_name = "groups/group_detail_override.html"
-    # paginate_by = None
-    # group = None
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
@@ -49,8 +39,8 @@ def sync_geoserver(request, group_slug: str):
     workspace_name = group_slug.replace(DEPARTMENT_GROUP_PROFILE_SUFFIX, '')
     redirect = request.POST.get('redirect')
     logger.debug(f'Workspace name: {workspace_name}')
-    logger.info(f'Would now send the geoserver sync task to the queue')
-    # task_sync_geoserver.delay(workspace_name, request.user.get_username())
+    logger.info(f'Sending geoserver sync task to the async queue...')
+    task_sync_geoserver.delay(workspace_name, request.user.get_username())
     messages.success(
         request, _(
             'GeoServer layers are being synced in the background. '
