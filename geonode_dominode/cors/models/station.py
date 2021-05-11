6a3186709f9df1f00ab112dc2af351a27dad8a6f
Wayner Barrios <waybarrios@gmail.com>
2021-05-11 12:19:58 -0500

    removing unused code

--- geonode_dominode/cors/models/station.py
+++ geonode_dominode/cors/models/station.py
@@ -36,14 +36,6 @@ class CORSStation(models.Model):
         indexes = self.indexfile_set.filter(
             date__gte=date_from, date__lt=date_to).order_by('date')
         txt_file = self.indexes_in_txt_filename(date_from, date_to)
-        list_file = list()
-        list_dates = list()
-        for index in indexes:
-            list_file.append(index.location)
-            list_dates.append(index.date)
-        with open(txt_file, 'w') as f:
-            for i in range(len(list_file)) :
-                f.write("{}  {}\n".format(list_dates[i],list_file[i]))
         return txt_file
 
     def get_indexes(self, date_from, date_to):
